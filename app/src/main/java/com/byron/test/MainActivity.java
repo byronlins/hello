3005929e5ba17b01ca3127ce62d7c416e83a2172
linbc <linbc@xiaopeng.com>
2018-07-27 17:31:53 +0800

    init

--- app/src/main/java/com/byron/test/MainActivity.java
+++ app/src/main/java/com/byron/test/MainActivity.java
@@ -6,6 +6,8 @@ import android.widget.ImageView;
 
 import com.byron.github01.ImageLoaderHelper;
 
+import test.byron.com.lib.MyClass;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
         ImageLoaderHelper.load(this,
               "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1533284634&di=b6d884ade6c552406f6e9f4c5b988896&imgtype=jpg&er=1&src=http%3A%2F%2Fe.hiphotos.baidu.com%2Fzhidao%2Fwh%253D680%252C800%2Fsign%3Da130fc56a451f3dec3e7b162acdedc27%2Fbf096b63f6246b60301586e8e0f81a4c510fa216.jpg",
                 iv);
+
+        MyClass.test();
     }
 }
---
+++ lib/src/main/java/test/byron/com/lib/MyClass.java
@@ -0,0 +1,8 @@
+package test.byron.com.lib;
+
+public class MyClass {
+
+    public static void test(){
+        System.out.print("haha");
+    }
+}
